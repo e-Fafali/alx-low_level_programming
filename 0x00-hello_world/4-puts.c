98695d1de4ffcacecb80ac4a73898465d5bda61c
e-Fafali <sandyagbottah@gmail.com>
2023-07-13 09:26:55 -0700

    4-puts.c: A c programme thay uses puts to print an output to terminal

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+/**
+ * Author: e-Fafali
+ * Purpose: use puts to display stuff
+ * filename: 4-puts.c
+*/
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts: prints an output to the terminal
+ *
+ * Return: (0) Sucess
+*/
+
+int main(void)
+{
+	char str[100] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(str);
+
+	return (0);
+}
