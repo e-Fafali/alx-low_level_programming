a0770fab7a1f37edacdb560f19db0bf84f56eb96
e-Fafali <sandyagbottah@gmail.com>
2023-07-13 15:18:05 -0700

    5-printf.c: A c prograamme that uses printf to print an output to the terminal

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main code
+ *
+ * printf: a function that outputs stuff to the terminal
+ *
+ * Return: Always (0) (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
