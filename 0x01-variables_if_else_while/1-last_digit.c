83ac5e4c86f8e1ce8fc786d65d89819c009d6859
e-Fafali <sandyagbottah@gmail.com>
2023-07-14 17:44:42 -0700

    a code to print the last digit of a number and determine if its less than 6 or 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+  * main - Main code 
+  * Description: a code to print the last digit of a number
+  * and determine if its less than 6 or 0
+  * Return: Always (0)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int q;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* if code */
+	q = n % 10;
+	if (q > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, q);
+	}
+	else if (q == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, q);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, q);
+	}
+
+	return (0);
+}
