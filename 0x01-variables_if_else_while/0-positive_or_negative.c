7a9aa8746ec4796b23dd42837247f90d84a6dbc2
e-Fafali <sandyagbottah@gmail.com>
2023-07-14 17:08:48 -0700

    0-positive_or_negative: a c code that assigns a random number and prints out whether its negative or positive or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+  * main - Main code
+  * Description: this code assigns a rundon number to n
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n <0.0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
