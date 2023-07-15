931377a4723ec96a6273ae7f4d08098c70069480
e-Fafali <sandyagbottah@gmail.com>
2023-07-14 19:22:55 -0700

    9-print_comb.c: Using putchar to print base 10 numbers from 0-9 seperated by & except 9.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - main code
+  *
+  * Description: Using putchar to print base 10 numbers
+  * from 0-9 seperated by ',' & ' ' except 9.
+  *
+  * putchar(): A function that prints out a character
+  *
+  * Return: Always (0)
+ */
+
+int main(void)
+{
+	char num = '0';
+
+	/* Usung a while loop to iterate ch*/
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
