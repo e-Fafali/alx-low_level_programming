2e26f93b77cb23c1c651d25e6cbc475be6b30748
e-Fafali <sandyagbottah@gmail.com>
2023-07-20 05:41:59 -0700

    2-print_alphabet_x10.c: a code the prints out alphabet set 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - as described
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int count = 0;
+	
+	while (count <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+ 		{
+ 			_putchar(alpha);
+ 		}
+		_putchar('\n');
+		count++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,6 @@ int _putchar(char);
 
 /* function to print out the alphabets in lower case*/
 void print_alphabet(void);
+
+/* functiont to print the alphabet set 10x*/
+void print_alphabet_x10(void);
