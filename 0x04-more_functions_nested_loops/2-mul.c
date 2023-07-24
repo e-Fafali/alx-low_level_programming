6d90c96f02611f5374ca219ceb8dad1a0e98e7f0
e-Fafali <sandyagbottah@gmail.com>
2023-07-23 17:02:55 -0700

    2-mul.c: code to multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - a function to perform multiplication
+ *
+ * @a: first variable to be multiplied
+ * @b: second variable to be multiplied
+ *
+ * Return: 1 or 0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,3 +6,6 @@ int _isupper(int c);
 
 /* Check if a value is a digit*/
 int _isdigit(int c);
+
+/* Multiply 2 numbers*/
+int mul(int a, int b);
