998682c6209d991469741338cd333d727da9767e
e-Fafali <sandyagbottah@gmail.com>
2023-07-23 21:10:47 -0700

    9-fizz_buzz.c: lets do weird stuff

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 )
+		if (i % 3 == 0 && i % 5 == 0 )
 		{
-			printf("fizz ");
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("buzz ");
+			printf("Buzz");
 		}
-		else 
+		else
 		{
 			printf("%d ", i);
 		}
