562f1b0191c4e00fd473a4177d7bb0b46d811de1
e-Fafali <sandyagbottah@gmail.com>
2023-08-01 14:02:33 -0700

    0-reset_to_98.c: A function to reset a number

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - a functio to reset a variable to 98
+ * 
+ * @n: a pointer variable
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
