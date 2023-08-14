bdeca0a25bb874d2ad5ee7d0f121bf1e6fde9a5d
e-Fafali <sandyagbottah@gmail.com>
2023-08-14 15:24:23 -0700

    2-main.c: a code to print the mane of a file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a code to print the mane of a file it was compiled from
+ *
+ * Return: 0;
+ */
+
+#define FILE __FILE__
+
+int main(void)
+{
+	printf("%s \n", FILE);
+
+	return (0);
+}
