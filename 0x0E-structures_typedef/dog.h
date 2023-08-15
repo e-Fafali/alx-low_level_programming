fc21df24b9de9fa094d652da022b21be5a43cd45
e-Fafali <sandyagbottah@gmail.com>
2023-08-14 19:12:17 -0700

    dog.h: Header file with info about dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - info on a dog
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member.
+ *
+ * Description - this a struct for information about a
+ * dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
