8d80ea27568a34257c3f19359e5e3b2babde689d
e-Fafali <sandyagbottah@gmail.com>
2023-08-17 05:35:53 +0000

    2-int_index.c: Comparing and saving stuff

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,17 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+/* Let's add come libraries */
+#include <stdio.h>
+#include <stddef.h>
+
 /* Fuction pointer */
 void print_name(char *name, void (*f)(char *));
 
 /* Let's get through some stuff */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* let's compare stuff */
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
