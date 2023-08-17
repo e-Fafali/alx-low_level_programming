f5da1d82ee089d2fe9bb0858d9aee196a20340c8
e-Fafali <sandyagbottah@gmail.com>
2023-08-17 05:43:06 +0000

    100-main-opcodes.c: lets write opcodes

---
+++ 0x0F-function_pointers/100-main-opcodes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, x;
+	char *vox;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	vox = (char *)main;
+
+	for (x = 0; x < bytes; x++)
+	{
+		if (i == bytes - 1)
+		{
+			/* let's do some thing weird*/
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
